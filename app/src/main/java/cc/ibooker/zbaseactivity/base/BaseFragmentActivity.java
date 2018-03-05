0c928fe902c2caad19eed0904740caf5c164fb1e
zrunker <zrunker@yahoo.com>
2018-03-05 18:54:11 +0800

    init Commit

---
+++ app/src/main/java/cc/ibooker/zbaseactivity/base/BaseFragmentActivity.java
@@ -0,0 +1,105 @@
+package cc.ibooker.zbaseactivity.base;
+
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
+import android.view.KeyEvent;
+import android.view.Window;
+
+import cc.ibooker.zbaseactivity.broadcastreceiver.NetBroadcastReceiver;
+import cc.ibooker.zbaseactivity.utils.ActivityUtil;
+
+/**
+ * BaseFragmentActivity是所有FragmentActivity的基类，把一些公共的方法放到里面，如基础样式设置，权限封装，网络状态监听等
+ * <p>
+ * Created by 邹峰立 on 2017/10/19.
+ */
+public abstract class BaseFragmentActivity extends FragmentActivity implements NetBroadcastReceiver.NetChangeListener {
+    public static NetBroadcastReceiver.NetChangeListener netEvent;// 网络状态改变监听事件
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // 隐藏标题栏
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        // 添加到Activity工具类
+        ActivityUtil.getInstance().addActivity(this);
+
+        // 初始化netEvent
+        netEvent = this;
+    }
+
+    @Override
+    protected void onDestroy() {
+        // Activity销毁时，提示系统回收
+        System.gc();
+        super.onDestroy();
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // 点击手机上的返回键，返回上一层
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            this.finish();
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    /**
+     * 权限检查方法，false代表没有该权限，ture代表有该权限
+     */
+    public boolean hasPermission(String... permissions) {
+        for (String permission : permissions) {
+            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    /**
+     * 权限请求方法
+     */
+    public void requestPermission(int code, String... permissions) {
+        ActivityCompat.requestPermissions(this, permissions, code);
+    }
+
+    /**
+     * 处理请求权限结果事件
+     *
+     * @param requestCode  请求码
+     * @param permissions  权限组
+     * @param grantResults 结果集
+     */
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        doRequestPermissionsResult(requestCode, grantResults);
+    }
+
+    /**
+     * 处理请求权限结果事件
+     *
+     * @param requestCode  请求码
+     * @param grantResults 结果集
+     */
+    public void doRequestPermissionsResult(int requestCode, @NonNull int[] grantResults) {
+    }
+
+    /**
+     * 网络状态改变时间监听
+     *
+     * @param netWorkState true有网络，false无网络
+     */
+    @Override
+    public void onNetChange(boolean netWorkState) {
+    }
+
+}
